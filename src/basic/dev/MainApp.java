3df2b7495ce2e3028b2c7e8510e354625f1b722a
Wiin <win.kinhvanhoa@gmail.com>
2022-03-30 10:20:53 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,23 @@
+package basic.dev;
+	
+public class MainApp {
+	public static void main(String[] args) {
+		/*
+		 * khai báo biến
+		 */
+		String firstName = "Nguyen Song";
+		String lastName = "Bao";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho va ten");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math:  " + markMath);
+		System.out.println("passed: " + passed);
+		
+//		khai báo Hằng, Hằng phải viết hoa
+		final float PI = 3.14f;
+		
+	}
+}
